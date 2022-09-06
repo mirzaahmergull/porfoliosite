51838888a8eebb81b676670304592e9e19a76eeb
mirzaahmergull <67520989+mirzaahmergull@users.noreply.github.com>
2022-09-06 23:17:22 +0000

    Deploying to gh-pages from  @ 9ae4056b2f4f0c4c00124426dbe39d6ca0f17bd1 🚀

---
+++ precache-manifest.7b3f1f54e114c9b217b58bdd0c78aea1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91138f833b83401114bc4cc26195217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9379604222d95427abc",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "d7756b94a42c56f8d500",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "5ce90eab512ac7a436ec",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "d1b0d8d416ed4e887632",
+    "url": "/static/js/2.55547e96.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.55547e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9379604222d95427abc",
+    "url": "/static/js/3.6e89626e.chunk.js"
+  },
+  {
+    "revision": "d7756b94a42c56f8d500",
+    "url": "/static/js/4.20324049.chunk.js"
+  },
+  {
+    "revision": "5ce90eab512ac7a436ec",
+    "url": "/static/js/main.000bf5b5.chunk.js"
+  },
+  {
+    "revision": "9a02234ea6c8b2767723",
+    "url": "/static/js/runtime-main.0c36ee15.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
